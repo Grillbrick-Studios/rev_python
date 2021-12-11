c566f0ab2d8510587ede6088091430d18076fea5
Jim Hessin <jhessin@gmail.com>
2021-12-11 07:17:50 -0700

    update private variables to be private

--- Timestamp.py
+++ Timestamp.py
@@ -3,6 +3,9 @@ if there is an update.
 
 >>> ts = Timestamp()
 Timestamp(...)
+
+>>> ts.needs_update
+True | False
 """
 
 import datetime
@@ -18,35 +21,35 @@ _FILENAME = 'timestamp.dat'
 
 class Timestamp:
     """ holds time data to be compared with the server """
-    _server_time: datetime.datetime
-    _local_time: datetime.datetime | None
+    __server_time: datetime.datetime
+    __local_time: datetime.datetime | None
 
     def __init__(self) -> None:
         request = requests.get(_URL)
         timestamp = request.json()['REV_Timestamp'][0]['timestamp']
-        self._server_time = datetime.datetime.fromisoformat(timestamp)
+        self.__server_time = datetime.datetime.fromisoformat(timestamp)
         self.load()
 
     @property
     def needs_update(self) -> bool:
-        return self._local_time is None or self._local_time < self._server_time
+        return self.__local_time is None or self.__local_time < self.__server_time
 
     @property
     def server_time(self):
         """ returns the server date for when the server data was last updated """
-        return self._server_time
+        return self.__server_time
 
     @property
     def local_time(self):
         """ returns the local_time from a file or None if there is none. """
-        return self._local_time
+        return self.__local_time
 
     def save(self):
         """ Save the server timestamp indicating that you have the most
         recent files """
         try:
             with open(_FILENAME, 'wb') as file:
-                pickle.dump(self._server_time, file, pickle.HIGHEST_PROTOCOL)
+                pickle.dump(self.__server_time, file, pickle.HIGHEST_PROTOCOL)
                 return True
         except (EnvironmentError, pickle.PicklingError) as err:
             print(f"{0}: export error: {1}".format(
@@ -57,8 +60,8 @@ class Timestamp:
         """ Load the local timestamp from a file if it exists """
         try:
             with open(_FILENAME, 'rb') as file:
-                self._local_time = pickle.load(file)
+                self.__local_time = pickle.load(file)
                 return True
         except (EnvironmentError, pickle.UnpicklingError):
-            self._local_time = None
+            self.__local_time = None
             return False
